2148d304cc60d955d8dbfcc7d9c9267450ebf6ed
Caique <caiqueonisto@live.com>
2020-08-10 20:35:30 -0300

    Fineshed desafio 10

--- src/components/Food/index.tsx
+++ src/components/Food/index.tsx
@@ -3,7 +3,8 @@ import React, { useState } from 'react';
 import { FiEdit3, FiTrash } from 'react-icons/fi';
 
 import { Container } from './styles';
-
+import '../../services/api';
+import api from '../../services/api';
 interface IFoodPlate {
   id: number;
   name: string;
@@ -27,11 +28,20 @@ const Food: React.FC<IProps> = ({
   const [isAvailable, setIsAvailable] = useState(food.available);
 
   async function toggleAvailable(): Promise<void> {
-    // TODO UPDATE STATUS (available)
+    try {
+      await api.put(`/foods/${food.id}`, {
+        ...food,
+        available: !isAvailable,
+      });
+
+      setIsAvailable(!isAvailable);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   function setEditingFood(): void {
-    // TODO - SET THE ID OF THE CURRENT ITEM TO THE EDITING FOOD AND OPEN MODAL
+    handleEditFood(food);
   }
 
   return (
